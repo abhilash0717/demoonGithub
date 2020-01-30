a32ae3424cb63161406070d19c8c16d84c0ba4de
abhilash0717 <abhilashabhi0717@gmail.com>
2020-01-30 11:49:16 +0000

    add app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/",function(req,res){
 app.get("/about",function(req,res){
 	res.render("about");
 });
-app.listen(3000, function(){
+app.listen(5000, function(){
 	console.log("server is running");
 });
